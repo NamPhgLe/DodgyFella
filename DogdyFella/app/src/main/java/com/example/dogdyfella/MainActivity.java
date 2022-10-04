cc8ad656b1ce6f13cf6feb42ae0f1cabd3244692
NamPhgLe <namphgle@gmail.com>
2022-10-03 17:42:35 -0700

    Game loop

--- DogdyFella/app/src/main/java/com/example/dogdyfella/MainActivity.java
+++ DogdyFella/app/src/main/java/com/example/dogdyfella/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.dogdyfella;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+      * MainActivity is entry point for application
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Set windows to full screen
+        Window window = getWindow();
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        //Set content view to game, so object can render to screen
+        setContentView(new Game(this));
+
+
     }
 }
